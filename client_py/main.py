8e79c4a1d4568c3967d8294aaa6e2090e7d52e0b
yyovchev <yordan.yovchev.1998@gmail.com>
2020-06-15 16:51:30 +0300

    add create contr func

--- client_py/main.py
+++ client_py/main.py
@@ -1,15 +1,60 @@
 import ibm_db
 
 conn = ibm_db.connect('DATABASE=SAMPLE;'
-                     'HOSTNAME=62.44.108.24;'  # 127.0.0.1 or localhost works if it's local
+                     'HOSTNAME=62.44.108.24;'
                      'PORT=50000;'
                      'PROTOCOL=TCPIP;'
                      'UID=db2admin;'
                      'PWD=db2admin;', '', '')
 
-stmt = ibm_db.exec_immediate(conn, "SELECT * FROM FN45398.STAFF")
-result = ibm_db.fetch_both(stmt)
+owners = []
+free_zones = []
 
-while( result ):
-    print(result['EGN']) # you get the idea
+def apply_on_row(query, f):
+    stmt = ibm_db.exec_immediate(conn,query)
     result = ibm_db.fetch_both(stmt)
+
+    while(result):
+        f(result)
+        result = ibm_db.fetch_both(stmt) 
+
+def fetch():
+    print("fetching Owners")
+
+    def fill(c):
+        return lambda row: c.append(row)
+    
+    apply_on_row("SELECT * FROM FN45398.OWNERS", fill(owners))
+    apply_on_row("SELECT * FROM FN45398.ZONES", fill(free_zones))
+
+def add_new_contr(): 
+    print("Add new contract\n 1. Choose owner: ")
+    print([owner["NAME"] for owner in owners])
+
+    input_name = input()
+    if len([name for name in owners if name["NAME"] == input_name]) == 0: 
+        print("name of owner not found")
+        return 
+    owner_id = [owner["OWNER_ID"] for owner in owners if owner["NAME"] == input_name][0]
+
+    print("Choose zone's id: ")
+    print(free_zones)
+    input_zone = int(input())
+    if len([zone for zone in free_zones if zone["ZONE_ID"] == input_zone]) == 0:
+        print("zone id not found")
+        return
+
+    ibm_db.exec_immediate(conn,"INSERT INTO FN45398.CONTRACTS (price, owner, zone_id, stdate, endate) VALUES (1001.99, "+ str(owner_id) +", "+ str(input_zone) +", CURRENT timestamp, CURRENT timestamp + 1 YEAR);")
+
+def main():
+
+    fetch()
+    ch = "Y"
+
+    while ch == "Y":
+        add_new_contr()
+        print("if you want to add ne contr pres Y")
+        ch = input()
+
+if __name__ == "__main__":
+    main()
